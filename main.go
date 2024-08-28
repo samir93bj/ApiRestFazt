ad5b924ba43ff1eec667cca51a0c45f37f1056dc
Samir Mahmud <samir93bj@gmail.com>
2024-08-28 01:37:11 -0300

    chore: Add initial project files and dependencies

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/samir93bj/go-gorm-restapi/routes"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
-}
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", handler)
+	router.HandleFunc("/", routes.HomeHandler)
 
 	http.ListenAndServe(":8080", router)
 	println("Server started on port 8080")
