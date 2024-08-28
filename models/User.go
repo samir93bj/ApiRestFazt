ab4ae78e06ff0a28eadb5321822ee5c1d94f7fa3
Samir Mahmud <samir93bj@gmail.com>
2024-08-28 18:56:34 -0300

    feat: Add email uniqueness constraint to User model

--- models/User.go
+++ models/User.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 
-	FirstName string
-	LastName  string
-	Email     string
+	FirstName string `gorm:"not null"`
+	LastName  string `gorm:"not null"`
+	Email     string `gorm:"uniqueIndex; not null"`
 	Tasks     []Task
 }
